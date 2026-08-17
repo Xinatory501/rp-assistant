46ceb0a9a75edf3f718e9734da4d17fbcedf012a
Xinatory501 <xinatory@users.noreply.github.com>
2026-08-17 22:27:28 +0300

    fix: import storage_service.dart in keyauth_service.dart

--- rp_assistant_flutter/lib/services/keyauth_service.dart
+++ rp_assistant_flutter/lib/services/keyauth_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'storage_service.dart';
 
 class AuthResult {
   final bool success;
